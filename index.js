28af9c1e47ffc7bf7e78f1f7e07c5b5080708ca7
Your Name <you@example.com>
2018-12-07 17:30:37 +0400

    bbb

---
+++ index.js
@@ -0,0 +1,45 @@
+// var os = require("os");
+// var message = "The platform is ";
+
+// function main(){
+//    console.log(message + os.platform());
+// }
+// main();
+
+
+// var express = require("express");
+// var app = express();
+
+// app.get("/", function(req, res){
+//     res.send("Hello world");
+// });
+
+// app.listen(3000, function(){
+//     console.log("Example is running on port 3000");
+// });
+
+
+
+
+// var express = require("express");
+// var app = express();
+
+// app.get("/", function(req, res){
+//     res.send("<h1> Hello world</h1>");
+// });
+
+// app.get("/name/:name", function (req, res){
+//     var n = req.params.name;
+//     res.send("<h1>Hello " + n +"</h1>");
+// });
+
+// app.get("/:surname/:name", function (req, res){
+//     var n = req.params.name;
+//     var s = req.params.surname;
+//     res.send("<h1>Hello " + s + n +"</h1>");
+// });
+
+// app.listen(3000, function(){
+//     console.log("Example is running on port 3000");
+// });
+
