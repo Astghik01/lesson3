71f2c22676297997468dea2c33182f7f2396c963
lusyapapoyan <you@example.com>
2018-12-08 18:27:26 +0400

    fs-i canotutyun

--- index.js
+++ index.js
@@ -43,3 +43,68 @@
 //     console.log("Example is running on port 3000");
 // });
 
+// slide 18 lesson 3
+// var Square = require("./module");
+// var mySquareObject = new Square(5);
+
+// function main() {
+//    console.log(mySquareObject.getArea());
+// }
+// main();
+
+
+// var fs = require('fs');
+
+// function main(){
+//    var file  = "hello.txt";
+//    fs.appendFileSync(file, "Hello world\n");
+// }
+// main();
+
+
+// var fs = require('fs');
+
+// function main(){
+//    fs.writeFile("hello.txt", "Hello world\n", function(err){
+//        console.log("fs.writeFile ended");
+//    });
+//    console.log("fs.writeFile");
+// }
+// main();
+
+
+// var fs = require('fs');
+
+// function main() {
+//    fs.writeFileSync("hello.txt", "Hello world\n");
+//    console.log("fs.writeFile");
+// }
+// main();
+
+
+// var fs = require('fs');
+// var dummyText = "Apple yep";
+
+// function main() {
+//    fs.writeFileSync("dummytext.txt", dummyText);
+//    var text = fs.readFileSync("dummytext.txt").toString();
+//    console.log(dummyText == text);
+//    console.log(text);
+//    fs.writeFileSync("undummytext.txt",
+//        text.replace("Apple", "Microsoft")
+//    );
+// }
+// main();
+
+
+var obj = 
+{
+    "first_name": "Vardan",
+    "last_name": "Hovsepyan",
+    "age": 13,
+    "tumo-student": true
+}
+var fs = require('fs');
+var myJSON = JSON.stringify(obj);
+        fs.writeFileSync("obj.json", myJSON);
+       
